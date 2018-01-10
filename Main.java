6d1218c3e77cf8e58cade5ddba40c828aa6d6e96
arnonagtzaam <34234780+arnonagtzaam@users.noreply.github.com>
2018-01-10 18:16:27 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,10 +1,15 @@
 
+import Repositories.SqlConnection;
 import GUI.UserInterface;
-
 import javax.swing.SwingUtilities;
+import java.sql.*;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        String connectionUrl = "jdbc:sqlserver://localhost\\SQLEXPRESS;databaseName=NetflixStatistix;integratedSecurity=true;";
+        connection.connectDatabase(connectionUrl);
         UserInterface ui = new UserInterface();
         SwingUtilities.invokeLater(ui);
 
